7e71a0748b342b89c9a99549f96abaa67985f0d1
Igor Trifunovic <igortridev@gmail.com>
2015-10-13 15:35:23 +0200

    JSHint process: clean code.

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,22 @@
-// Test watchify...
-
 var infosImage = [
-  {'id': '0', 'src': 'images/bird.jpg', 'title': 'Bird'},
-  {'id': '1', 'src': 'images/red_bird.jpg', 'title': 'Red bird'},
-  {'id': '2', 'src': 'images/spring.jpg', 'title': 'Spring'},
-  {'id': '3', 'src': 'images/winter.jpg', 'title': 'Winter'}
+	{'id': '0', 'src': 'images/bird.jpg', 'title': 'Bird'},
+	{'id': '1', 'src': 'images/red_bird.jpg', 'title': 'Red bird'},
+	{'id': '2', 'src': 'images/spring.jpg', 'title': 'Spring'},
+	{'id': '3', 'src': 'images/winter.jpg', 'title': 'Winter'}
 ];
 
 var infosMiniatures = [
-  {'id': '0', 'src': 'images/m_photo1.png', 'title': 'Bird'},
-  {'id': '1', 'src': 'images/m_photo2.png', 'title': 'Red bird'},
-  {'id': '2', 'src': 'images/m_photo3.png', 'title': 'Spring'},
-  {'id': '3', 'src': 'images/m_photo4.png', 'title': 'Winter'}
+	{'id': '0', 'src': 'images/m_photo1.png', 'title': 'Bird'},
+	{'id': '1', 'src': 'images/m_photo2.png', 'title': 'Red bird'},
+	{'id': '2', 'src': 'images/m_photo3.png', 'title': 'Spring'},
+	{'id': '3', 'src': 'images/m_photo4.png', 'title': 'Winter'}
 ];
 
 var containerDots = document.getElementById('dots'),
-  dots = containerDots.getElementsByTagName('li'),
-  containerImage = document.getElementById("container"),
-  nav = document.getElementById('nav'),
-  arrows = nav.getElementsByTagName('a');
+		dots = containerDots.getElementsByTagName('li'),
+		containerImage = document.getElementById("container"),
+		nav = document.getElementById('nav'),
+		arrows = nav.getElementsByTagName('a');
 
 var secDuration = 3, maxImages = infosImage.length, timer, delay, isPlayed = false;
 
@@ -26,13 +24,13 @@ var secDuration = 3, maxImages = infosImage.length, timer, delay, isPlayed = fal
 
 // Créer autant d'éléments DOM que d'images
 
-function setAttributes(tagName, attributes, data){
-  var elmt = document.createElement(tagName);
-  for(var i = 0; i< attributes.length; i++){
-    elmt.setAttribute(attributes[i], data[i]);
-  }
-  //console.log(elmt.className);
-  return elmt;
+function setAttributes(tagName, attributes, data) {
+	var elmt = document.createElement(tagName);
+	for (var i = 0; i < attributes.length; i++) {
+		elmt.setAttribute(attributes[i], data[i]);
+	}
+	//console.log(elmt.className);
+	return elmt;
 }
 
 // La méthode String.prototype.match() permet d'obtenir le tableau des correspondances entre la chaîne courante et une expression rationnelle
@@ -43,7 +41,7 @@ function setAttributes(tagName, attributes, data){
 // On dit que ce lien à la classe dont l'expression comporte le chiffre recherché possède la classe "active"
 
 /*var str = 'image0';
-var chiffre = '0'; */// ou var chiffre = 0, tester la différence (replace 0 by current index in loop for)
+ var chiffre = '0'; */// ou var chiffre = 0, tester la différence (replace 0 by current index in loop for)
 // Utiliser la syntaxe new RegExp(variable valueToFind, 'flag') et non /[variable valueToFind]/g
 // Préciser le flag (2nd paramètre de RegExp sinon pas de valeurs multiples trouvées renvoyées, uniquement la première)
 //var regexp = new RegExp(chiffre, 'g'); 
@@ -60,180 +58,185 @@ var chiffre = '0'; */// ou var chiffre = 0, tester la différence (replace 0 by 
 //console.log(toto);
 //console.log(String.prototype);
 
-function addClass(item){
-  // Find/Active className
-    var correspondance = [];
-    var chiffre = getId();
-    var str = document.getElementsByTagName('dl')[0].getAttribute('class');
-    var regexp = new RegExp(chiffre, 'g');
-    correspondance = str.match(regexp).toString();
-    for(var j = 0; j < dots.length; j++){
-      item = infosImage[j];
-      if(item.id == correspondance){
-        dots[j].className = ' ';
-        dots[j].className = dots[j].className + ' active';
-      }else{
-        dots[j].className = ' ';
-        dots[j].className = dots[j].className + ' inactive';
-      }
-      
-    }
+function addClass(item) {
+	// Find/Active className
+	var correspondance = [];
+	var chiffre = getId();
+	var str = document.getElementsByTagName('dl')[0].getAttribute('class');
+	var regexp = new RegExp(chiffre, 'g');
+	correspondance = str.match(regexp).toString();
+	for (var j = 0; j < dots.length; j++) {
+		item = infosImage[j];
+		if (item.id === correspondance) {
+			dots[j].className = ' ';
+			dots[j].className = dots[j].className + ' active';
+		} else {
+			dots[j].className = ' ';
+			dots[j].className = dots[j].className + ' inactive';
+		}
+
+	}
 }
 
 
 
 
-function createElements(json, container, bool, len, index){
-  if(!bool){
-    // Create <image> element
-    //console.log(index);
-    // Delete old image
-    while (containerImage.firstChild) {
-      containerImage.removeChild(containerImage.firstChild);
-    }
-    // Put new image
-    // Elément <DIV>
-    var link = setAttributes("dl", ["id", "class"], [json[index].id, "image" + json[index].id]);
-    container.appendChild(link);
-    // Eléments <SPAN>
-    var desc = document.createElement("dt");
-    // Attribute title text
-    desc.innerText = json[index].title;
-    link.appendChild(desc);
-    var content = document.createElement("dd");
-    link.insertBefore(content, desc.nextSibling);
-    // Eléments <IMG>
-    // Attribute image source
-    var photo = setAttributes("img", ["src", "alt"], [json[index].src, json[index].title]);
-    content.appendChild(photo);
-  }else{
-    // Create <dots> elements
-    for(var i = 0; i < len; i++){
-      // Eléments <LI>
-      var link = setAttributes("li", ["class"], ["link" + json[i].id]);
-      container.appendChild(link);
-      // Eléments <A>
-      var desc = setAttributes("a", ["id", "class", "href", "title"], ["desc" + json[i].id, "desc" + json[i].id, "", json[i].title]);
-      // Attribute title text
-      desc.innerText = json[i].title;
-      link.appendChild(desc);
-      // Eléments <IMG>
-      // Attribute image source
-      var photo = setAttributes("img", ["src", "alt"], [json[i].src, json[i].title]);
-      desc.appendChild(photo);
-    }
-  }
-  // Relance le player avec un timer réinitialisé
-  if(isPlayed){
-    pause();
-    play();
-  }
+function createElements(json, container, bool, len, index) {
+	var link, desc, photo;
+	if (!bool) {
+		// Create <image> element
+		//console.log(index);
+		// Delete old image
+		while (containerImage.firstChild) {
+			containerImage.removeChild(containerImage.firstChild);
+		}
+		// Put new image
+		// Elément <DIV>
+		link = setAttributes("dl", ["id", "class"], [json[index].id, "image" + json[index].id]);
+		container.appendChild(link);
+		// Eléments <SPAN>
+		desc = document.createElement("dt");
+		// Attribute title text
+		desc.innerText = json[index].title;
+		link.appendChild(desc);
+		var content = document.createElement("dd");
+		link.insertBefore(content, desc.nextSibling);
+		// Eléments <IMG>
+		// Attribute image source
+		photo = setAttributes("img", ["src", "alt"], [json[index].src, json[index].title]);
+		content.appendChild(photo);
+	} else {
+		// Create <dots> elements
+		for (var i = 0; i < len; i++) {
+			// Eléments <LI>
+			link = setAttributes("li", ["class"], ["link" + json[i].id]);
+			container.appendChild(link);
+			// Eléments <A>
+			desc = setAttributes("a", ["id", "class", "href", "title"], ["desc" + json[i].id, "desc" + json[i].id, "", json[i].title]);
+			// Attribute title text
+			desc.innerText = json[i].title;
+			link.appendChild(desc);
+			// Eléments <IMG>
+			// Attribute image source
+			photo = setAttributes("img", ["src", "alt"], [json[i].src, json[i].title]);
+			desc.appendChild(photo);
+		}
+	}
+	// Relance le player avec un timer réinitialisé
+	if (isPlayed) {
+		pause();
+		play();
+	}
 }
 
 
 
-function getId(){
-  var id = document.getElementsByTagName('dl')[0].attributes.getNamedItem('id').nodeValue; // OU document.getElementsByTagName('dl')[0].getAttribute('id')
-  return id;
+function getId() {
+	var id = document.getElementsByTagName('dl')[0].attributes.getNamedItem('id').nodeValue; // OU document.getElementsByTagName('dl')[0].getAttribute('id')
+	return id;
 }
 
 
 // On crée une fonction appelée dans le callback du gestionnaire d'événement plus bas
 // afin de créer une closure => portée en dehors du gestionnaire d'événements
-function callbackForId(item){
-  return function(e){ // Closure to keep each item in a local and own scope
-    e.preventDefault(); // prevent links <a> default action
-    createElements(infosImage, containerImage, false, maxImages, item.id);
-    addClass(infosImage);
-  }
+function callbackForId(item) {
+	return function(e) { // Closure to keep each item in a local and own scope
+		e.preventDefault(); // prevent links <a> default action
+		createElements(infosImage, containerImage, false, maxImages, item.id);
+		addClass(infosImage);
+	};
 }
 
 // Externalisons la callback dans une fonction callbackForId qui sera une fermeture
-function getImage(){
-  createElements(infosMiniatures, containerDots, true, maxImages);
-  createElements(infosImage, containerImage, false, maxImages, 0);
-  addClass(infosImage);
-  
-  //console.log(dots);
-  for(var i = 0; i < maxImages; i++){
-    //console.log(dots[i].attributes.getNamedItem('class').nodeValue);
-    var item = infosImage[i];
-    dots[i].addEventListener('click', callbackForId(item), false);
-  }
+function getImage() {
+	createElements(infosMiniatures, containerDots, true, maxImages);
+	createElements(infosImage, containerImage, false, maxImages, 0);
+	addClass(infosImage);
+
+	//console.log(dots);
+	for (var i = 0; i < maxImages; i++) {
+		//console.log(dots[i].attributes.getNamedItem('class').nodeValue);
+		var item = infosImage[i];
+		dots[i].addEventListener('click', callbackForId(item), false);
+	}
 }
 
 // Pas d'externalisation de fonction, mais une déclaration de IIFE
-function getImage2(){
-  createElements(infosMiniatures, containerDots, true, maxImages);
-  createElements(infosImage, containerImage, false, maxImages, 0);
-  addClass(infosImage);
-  for(var i = 0; i < maxImages; i++){
-    //console.log(dots[i].attributes.getNamedItem('class').nodeValue);
-    var item = infosImage[i];
-    dots[i].addEventListener('click', (function(item){ // IIFE to encapsulate code
-        return function(e){ // Closure to keep each item in a local and own scope
-          e.preventDefault(); // prevent links <a> default action
-          createElements(infosImage, containerImage, false, maxImages, item.id);
-          addClass(infosImage);
-        }
-      })(item) // Invoke immediately function
-      ,false);
-  }
+//function getImage2() {
+//	createElements(infosMiniatures, containerDots, true, maxImages);
+//	createElements(infosImage, containerImage, false, maxImages, 0);
+//	addClass(infosImage);
+//	for (var i = 0; i < maxImages; i++) {
+//		//console.log(dots[i].attributes.getNamedItem('class').nodeValue);
+//		var item = infosImage[i];
+//		dots[i].addEventListener('click', (function(item) { // IIFE to encapsulate code
+//			return function(e) { // Closure to keep each item in a local and own scope
+//				e.preventDefault(); // prevent links <a> default action
+//				createElements(infosImage, containerImage, false, maxImages, item.id);
+//				addClass(infosImage);
+//			};
+//		// Invoke immediately function
+//		})(item), false);
+//	}
+//}
+
+
+function next() {
+	var index = getId();
+	index++;
+	if (index >= maxImages) {
+		index = 0;
+	} 
+//	else {
+//		index = index;
+//	}
+	createElements(infosImage, containerImage, false, maxImages, index);
+	addClass(infosImage);
+	// Relance le player avec un timer réinitialisé
+	if (isPlayed) {
+		pause();
+		play();
+	}
 }
 
-
-function next(){
-  var index = getId();
-  index++;
-  if(index >= maxImages){
-    index = 0;
-  }else{
-    index = index;
-  }
-  createElements(infosImage, containerImage, false, maxImages, index);
-  addClass(infosImage);
-  // Relance le player avec un timer réinitialisé
-  if(isPlayed){
-    pause();
-    play();
-  }
-}
-
-function prev(){
-  var index = getId();
-  index--;
-  if(index < 0){
-    index = maxImages - 1;
-  }else{
-    index = index;
-  }
-  createElements(infosImage, containerImage, false, maxImages, index);
-  addClass(infosImage);
-  // Relance le player avec un timer réinitialisé
-  if(isPlayed){
-    pause();
-    play();
-  }
+function prev() {
+	var index = getId();
+	index--;
+	if (index < 0) {
+		index = maxImages - 1;
+	} 
+//	else {
+//		index = index;
+//	}
+	createElements(infosImage, containerImage, false, maxImages, index);
+	addClass(infosImage);
+	// Relance le player avec un timer réinitialisé
+	if (isPlayed) {
+		pause();
+		play();
+	}
 }
 
 // La fonction qui "joue" les images
 function play() {
-  // Un setTimeout pour lancer l'image suivante
-  delay = setTimeout("next()", secDuration * 1000);
-  // Un setTimeout pour lancer le play global (récursivité)
-  timer = setTimeout("play()", secDuration * 1000);
-  return isPlayed = true;
+	// Un setTimeout pour lancer l'image suivante
+	delay = setTimeout(next, secDuration * 1000);
+	// Un setTimeout pour lancer le play global (récursivité)
+	timer = setTimeout(play, secDuration * 1000);
+	isPlayed = true;
+	return isPlayed;
 }
 
-function pause(){
-  clearTimeout(delay);
-  clearTimeout(timer);
-  delay = null;
-  timer = null;
-  return isPlayed = false;
+function pause() {
+	clearTimeout(delay);
+	clearTimeout(timer);
+	delay = null;
+	timer = null;
+	isPlayed = false;
+	return isPlayed;
 }
 
-document.addEventListener('load', getImage2(), false);
+document.addEventListener('load', getImage(), false);
 document.getElementById("next").addEventListener('click', next, false);
 document.getElementById("prev").addEventListener('click', prev, false);
 document.getElementById("play").addEventListener('click', play, false);
